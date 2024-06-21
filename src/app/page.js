c209e4b76ac900da9b77933f6ac52b7fcce99af8
cacosat <86966872+cacosat@users.noreply.github.com>
2024-06-21 18:57:33 -0400

    stop input logic

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@
 import React from "react";
 import {useState, useEffect} from 'react';
 import Image from "next/image"; 
+import Link from "next/link";
 import {
   Card,
   CardContent,
@@ -17,19 +18,35 @@ import {
   InputOTPSeparator,
   InputOTPSlot,
 } from "@/components/ui/input-otp"
+import InputOtpCustom from "@/components/ui/input-otp-custom";
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/modeToggle";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export default function Home() {
+  const validationRegex = /^[0-9a-zA-Z]+$/;
   const [otp, setOtp] = useState('');
 
   const handleOtpChange = (newOtp) => {
     if (newOtp.length <= 6 || newOtp.length < otp.length) {  // Allow changes that reduce the length
       setOtp(newOtp);  // Set the new OTP directly
+      console.log(newOtp);
     }
   };
+
+  const handleSubmit = (event, redirect) => {
+    const value = otp; // dependent on state hook otp
+    if (value.length >= 3) {
+      if (!isNaN(parseInt(value[0])) || !isNaN(parseInt(value[1]))) {
+        console.log("The first two characters must be letters");
+      } else {
+        console.log("correct")
+      }
+    } else {
+      console.log("Ingresa un valor del tipo 'pc1000, pa251, pa10, etc.'")
+    }
+  }
 
   return (
     <main className="flex flex-col h-screen min-w-[300px] max-w-[1440px] mx-auto pt-10">
@@ -53,22 +70,12 @@ export default function Home() {
                 <CardDescription>Ingresa el número de paradero (ej.: PA100)</CardDescription>
               </CardHeader>
               <CardContent className="self-center">
-                <InputOTP maxLength={6} value={otp} onChange={handleOtpChange}>
-                  <InputOTPGroup>
-                    <InputOTPSlot index={0}  />
-                    <InputOTPSlot index={1}  />
-                  </InputOTPGroup>
-                  <InputOTPSeparator />
-                  <InputOTPGroup>
-                      <InputOTPSlot index={2} />
-                      <InputOTPSlot index={3} />
-                      <InputOTPSlot index={4} />
-                      <InputOTPSlot index={5} />
-                  </InputOTPGroup>
-                </InputOTP>
+
+                <InputOtpCustom pattern={validationRegex} maxLength={6} value={otp} onChange={handleOtpChange} />
+
               </CardContent>
               <CardFooter className="flex justify-end">
-                <Button className="hover:bg-neutral-950 active:bg-neutral-950 transition-all">Buscar</Button>
+                <Button onClick={handleSubmit} className="hover:bg-neutral-950 active:bg-neutral-950 transition-all">Buscar</Button>
               </CardFooter>
             </TabsContent>
             <TabsContent value="bus" className="flex flex-col">
@@ -89,19 +96,7 @@ export default function Home() {
                 <CardDescription>Ingresa el número de paradero (ej.: PA100)</CardDescription>
               </CardHeader>
               <CardContent className="self-center">
-                <InputOTP maxLength={6}>
-                  <InputOTPGroup>
-                    <InputOTPSlot index={0} />
-                    <InputOTPSlot index={1} />
-                  </InputOTPGroup>
-                  <InputOTPSeparator />
-                  <InputOTPGroup>
-                    <InputOTPSlot index={2} />
-                    <InputOTPSlot index={3} />
-                    <InputOTPSlot index={4} />
-                    <InputOTPSlot index={5} />
-                  </InputOTPGroup>
-                </InputOTP>
+                <InputOtpCustom pattern={validationRegex} value={otp} onChange={handleOtpChange} />
               </CardContent>
               <CardHeader>
                 <CardTitle>Bus</CardTitle>
@@ -111,7 +106,9 @@ export default function Home() {
                 <Input />
               </CardContent>
               <CardFooter className="flex justify-end">
-                <Button className="hover:bg-neutral-950 active:bg-neutral-950 transition-all">Buscar</Button>
+                <Button onClick={handleSubmit} className="hover:bg-neutral-950 active:bg-neutral-950 transition-all">
+                  <p>Buscar</p>
+                </Button>
               </CardFooter>
             </TabsContent>
           </Tabs>
---
+++ src/components/ui/input-otp-custom.jsx
@@ -0,0 +1,28 @@
+"use client"
+
+import React from "react";
+import {
+    InputOTP,
+    InputOTPGroup,
+    InputOTPSeparator,
+    InputOTPSlot,
+  } from "@/components/ui/input-otp"
+
+export default function InputOtpCustom(props) {
+
+    return <>
+            <InputOTP maxLength={6} pattern={props.pattern} value={props.value} onChange={props.onChange}>
+                <InputOTPGroup>
+                    <InputOTPSlot index={0}  />
+                    <InputOTPSlot index={1}  />
+                </InputOTPGroup>
+                <InputOTPSeparator />
+                <InputOTPGroup>
+                    <InputOTPSlot index={2} />
+                    <InputOTPSlot index={3} />
+                    <InputOTPSlot index={4} />
+                    <InputOTPSlot index={5} />
+                </InputOTPGroup>
+            </InputOTP>
+    </>
+}
