93576cd7b637ece73454b386c422b2a6a1b8fe87
cacosat <86966872+cacosat@users.noreply.github.com>
2024-06-23 22:18:28 -0400

    update

--- src/app/result/page.js
+++ src/app/result/page.js
@@ -85,78 +85,82 @@ export default function ResultPage() {
         }
     }
 
-    return <div className="min-h-screen flex flex-col gap-12 justify-center items-center">
-        <div className="flex flex-col gap-8">
-            <Card className="flex flex-col gap-8">
-                <CardHeader className="p-0">
-                    <Suspense>
-                        <CardTitle className="text-4xl font-semibold tracking-wide">{parsed_stop.toUpperCase()}</CardTitle>
-                    </Suspense>
-                </CardHeader>
-                <div className="w-[75%] border-t-[2px] border-black/10 dark:border-white/10 rounded-full"></div>
-                <CardContent className="flex flex-col p-0 gap-4">
-                    <Suspense>
-                        <p className="text-neutral-500 font-light text-sm">{`${active_routes} recorridos: `}</p>
-                        <div className="flex gap-2 flex-wrap">
-                            {parsed_results.map((item, index) => {
-                                return <BadgeCustom key={index} content={item.route_id} edit={editBadge} className={`rounded-full w-fit`} />
-                            })}
-                        </div>
-                    </Suspense>
-                </CardContent>
-            </Card>
+    return (
+    <Suspense>
+        <div className="min-h-screen flex flex-col gap-12 justify-center items-center">
             <div className="flex flex-col gap-8">
-                <Suspense>
-                    {parsed_results.map((route, index) => {
-                        const buses = route.incoming;
-                        return (
-                        <div key={route.route_id} className="flex flex-col gap-2">
-                            <div className="flex gap-1 items-center">
-                                <Image src="/micro_black.png" alt="bus icon" width={32} height={32} className="dark:invert" />
-                                <p className="text-2xl font-semibold" key={index}>{route.route_id}</p>
+                <Card className="flex flex-col gap-8">
+                    <CardHeader className="p-0">
+                        <Suspense>
+                            <CardTitle className="text-4xl font-semibold tracking-wide">{parsed_stop.toUpperCase()}</CardTitle>
+                        </Suspense>
+                    </CardHeader>
+                    <div className="w-[75%] border-t-[2px] border-black/10 dark:border-white/10 rounded-full"></div>
+                    <CardContent className="flex flex-col p-0 gap-4">
+                        <Suspense>
+                            <p className="text-neutral-500 font-light text-sm">{`${active_routes} recorridos: `}</p>
+                            <div className="flex gap-2 flex-wrap">
+                                {parsed_results.map((item, index) => {
+                                    return <BadgeCustom key={index} content={item.route_id} edit={editBadge} className={`rounded-full w-fit`} />
+                                })}
                             </div>
-                            <Card className="p-0 overflow-hidden">
-                                <Table >
-                                    <TableBody className="">
-                                        {buses.map((bus, index) => {
-                                            return <TableRow key={bus.bus_plate_number} className="border-b dark:border-white/10 text-sm">
-                                                <TableCell className="font-medium">
-                                                    {bus.bus_distance === null ? (
-                                                        <Image src="/micro_fuera.png" alt="icono bus" height={32} width={32} />
-                                                        // <p className="">-</p>
-                                                    ) : (
-                                                        <div className="flex gap-2 items-center justify-start">
-                                                            <p>{bus.bus_plate_number}</p>
-                                                        </div>
-                                                    )}
-                                                </TableCell>
-                                                <TableCell className="flex gap-2 h-[45px] justify-center items-center">
-                                                        {bus.bus_distance >= 3000 ? (
-                                                                <p>{`${bus.bus_distance} mts.`}</p>
-                                                        ) : ( bus.bus_distance === null ? (
-                                                            // <Image src="/micro_fuera.png" alt="icono bus" height={32} width={32} />
-                                                            <p className="">-</p>
+                        </Suspense>
+                    </CardContent>
+                </Card>
+                <div className="flex flex-col gap-8">
+                    <Suspense>
+                        {parsed_results.map((route, index) => {
+                            const buses = route.incoming;
+                            return (
+                            <div key={route.route_id} className="flex flex-col gap-2">
+                                <div className="flex gap-1 items-center">
+                                    <Image src="/micro_black.png" alt="bus icon" width={32} height={32} className="dark:invert" />
+                                    <p className="text-2xl font-semibold" key={index}>{route.route_id}</p>
+                                </div>
+                                <Card className="p-0 overflow-hidden">
+                                    <Table >
+                                        <TableBody className="">
+                                            {buses.map((bus, index) => {
+                                                return <TableRow key={bus.bus_plate_number} className="border-b dark:border-white/10 text-sm">
+                                                    <TableCell className="font-medium">
+                                                        {bus.bus_distance === null ? (
+                                                            <Image src="/micro_fuera.png" alt="icono bus" height={32} width={32} />
+                                                            // <p className="">-</p>
                                                         ) : (
-                                                            <div className="flex gap-2 items-center">
-                                                                <Progress value={progressToStop(bus.bus_distance, 3000)} className="h-[3px] min-w-[40px]" />
-                                                                <p className=" text-nowrap">{bus.bus_distance} mts.</p>
+                                                            <div className="flex gap-2 items-center justify-start">
+                                                                <p>{bus.bus_plate_number}</p>
                                                             </div>
-                                                        )
                                                         )}
-                                                </TableCell>
-                                                <TableCell>
-                                                    <div className="flex justify-end">{checkInputString(bus.arrival_estimation)}</div>
-                                                </TableCell>
-                                            </TableRow>
-                                        })}
-                                    </TableBody>
-                                </Table>
-                            </Card>
-                        </ div>
-                        )
-                    })}
-                </Suspense>
+                                                    </TableCell>
+                                                    <TableCell className="flex gap-2 h-[45px] justify-center items-center">
+                                                            {bus.bus_distance >= 3000 ? (
+                                                                    <p>{`${bus.bus_distance} mts.`}</p>
+                                                            ) : ( bus.bus_distance === null ? (
+                                                                // <Image src="/micro_fuera.png" alt="icono bus" height={32} width={32} />
+                                                                <p className="">-</p>
+                                                            ) : (
+                                                                <div className="flex gap-2 items-center">
+                                                                    <Progress value={progressToStop(bus.bus_distance, 3000)} className="h-[3px] min-w-[40px]" />
+                                                                    <p className=" text-nowrap">{bus.bus_distance} mts.</p>
+                                                                </div>
+                                                            )
+                                                            )}
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        <div className="flex justify-end">{checkInputString(bus.arrival_estimation)}</div>
+                                                    </TableCell>
+                                                </TableRow>
+                                            })}
+                                        </TableBody>
+                                    </Table>
+                                </Card>
+                            </ div>
+                            )
+                        })}
+                    </Suspense>
+                </div>
             </div>
         </div>
-    </div>
+    </Suspense>
+    )
 }
